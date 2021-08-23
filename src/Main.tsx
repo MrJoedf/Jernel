4ce2a7afaf62c5166c93fd1785b937e1060369df
MrJoedf <joefoster0011@gmail.com>
2021-08-23 17:05:34 -0500

    Style change

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,15 @@
 import React, {useState, useEffect, Dispatch, SetStateAction} from 'react';
 import './App.scss';
 
+
+//---PROP RELATED--//
+
+//---PROP FUNCTIONS--//
 type PropFunctionBool = Dispatch<SetStateAction<boolean>>;
 type PropFunctionString = Dispatch<SetStateAction<string>>;
 type PropFunction  = Dispatch<SetStateAction<void>>;
 
+//PROP OBJECTS INTERFACE
 interface PropObjects{
     handleLogout: PropFunction;
     userID: string;
@@ -13,8 +18,10 @@ interface PropObjects{
 export default function Main(PropObjs : PropObjects){
     return(
         <div className="main"> 
-        <h2>This is main.</h2>
-        <button onClick={()=>PropObjs.handleLogout()}>Log out</button>
+            <h2>This is main.</h2>
+            <button 
+                onClick={()=>PropObjs.handleLogout()}>Log out
+            </button>
         </div>
        
     )
